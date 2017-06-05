23e934855e6f80d64c195f15bc46cebfd31afbdc
XIu-Chi Chang <s94040511s@gmail.com>
2017-06-05 22:31:15 +0800

    Add# setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'EmailboxPy',
+    version = '0.0.1',
+    description = 'E Mailbox System on Raspberry pi',
+    classifiers = [
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+    ],
+    author = 'floydxiu, AristoChen',
+    packages = ['API'],
+    install_requires = []
+)
