74f87d9eb25874e08df46fed6cef002f8b75353f
XIu-Chi Chang <s94040511s@gmail.com>
2017-06-06 12:00:10 +0800

    Add# Untracked File API/Config.py

---
+++ API/Config.py
@@ -0,0 +1,9 @@
+#PBKDF2_HMAC Setting
+HASHING_ALGORITHM = 'sha256'
+HASHING_SALT = 'Just_For_Test'
+HASHING_TIMES = 500
+
+#Gmail Account for Sending System Email
+#You need to set up account id and password
+GMAIL_ACCOUNT = None
+CMAIL_PASSWORD = None
